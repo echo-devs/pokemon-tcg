835336fc3e9d46cd607feae3ec7c8ec4a683d87e
Kirk Wang <k3wang@gmail.com>
2022-03-05 14:57:57 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  devise_for :users
+  get '*path',
+      to: 'home#index',
+      constraints: ->(request) { request.format.html? }
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
